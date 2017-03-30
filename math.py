26868b4351d9e67a782a4277f3d6abca92a9ff96
Drake Thomas <drakethomas550@gmail.com>
2017-03-29 21:53:00 -0400

    fixed functions and added testLoop
    testLoop goes through all the other smaller functions. and fixed other function added return

--- math.py
+++ math.py
@@ -1,15 +1,19 @@
-def add(x, y): x + y
-def sub(x, y): x - y
-def mul(x, y): pass
-def div(x, y): pass
-def div(x, y): pass
+def add(x, y): return x + y
+def sub(x, y): return x - y
+def mul(x, y): return x * y
+def div(x, y): return x / y
+def mod(x, y): return x % y
 
+def testLoop():
+	for i in range(11): print(add(i, i))
 
-for i in range(11): print(add(i, i))
+	for j in range(11): print(sub(i, i))
 
-for j in range(11): print(sub(i, i))
+	for x in range(11): print(mul(i, i))
 
-for x in range(11): print(mul(i, i))
+	for y in range(11): print(div(i, i))
 
-for y in range(11): print(div(i, i))
+	for l in range(11): print(mul(i, i))
+
+	for p in range(11): print(mod(i, i))
 
