bcd666f05505d90e3818b14593fd2419a5a872db
Drake Thomas <drakethomas550@gmail.com>
2017-03-30 13:06:55 -0400

    added other functions

--- math.py
+++ math.py
@@ -8,18 +8,11 @@ def Mod(x, y): return x % y
 def All(x): 
 	try: return x + x - x * x / x % x // x * x + x
 	except: return 'error'
-def testLoop():
-	addR = [Add(i, i) for i in range(11)]
-	subR = [Sub(i, i) for i in range(11)]
-	mulR = [Mul(i, i) for i in range(11)]
-	divR = [Div(i, i) for i in range(11)]
-	AllR = [All(i) for i in range(11)]
 
-	print(addR)
-	print(subR)
-	print(mulR)
-	print(divR)
-	print("results")
+# all math functions
 
-testLoop()
+# other functions
+def helloW(): print("Hello World!")
+def byeW(): print("Goodbye World!")
+
  
