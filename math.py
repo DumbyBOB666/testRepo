fffc115646a81fa580c4271ac6e61aca4950deec
Drake Thomas <drakethomas550@gmail.com>
2017-03-30 13:09:18 -0400

    loopFunc
    loopFunc well allow you test other functions, give it a range and a function to test > loopFunc(10, Div)

--- math.py
+++ math.py
@@ -8,18 +8,9 @@ def Mod(x, y): return x % y
 def All(x): 
 	try: return x + x - x * x / x % x // x * x + x
 	except: return 'error'
-def testLoop():
-	addR = [Add(i, i) for i in range(11)]
-	subR = [Sub(i, i) for i in range(11)]
-	mulR = [Mul(i, i) for i in range(11)]
-	divR = [Div(i, i) for i in range(11)]
-	AllR = [All(i) for i in range(11)]
 
-	print(addR)
-	print(subR)
-	print(mulR)
-	print(divR)
-	print("results")
+def loopFunc(x, func):
+	for i in range(x):
+		print(func(i, i))
 
-testLoop()
- 
+loopFunc(11, Div)
