ac4ef86f12bad2987bda59e579dc4083795b4abc
Drake Thomas <drakethomas550@gmail.com>
2017-03-29 22:03:56 -0400

    made All()
    All does everything

--- math.py
+++ math.py
@@ -3,15 +3,19 @@ def sub(x, y): return x - y
 def mul(x, y): return x * y
 def div(x, y): return x / y
 def mod(x, y): return x % y
+def All(x): return x + x - x * x / x % x // x * x + x
 
 def testLoop():
 	add = [add(i, i) for i in range(11)]
 	sub = [sub(i, i) for i in range(11)]
 	mul = [mul(i, i) for i in range(11)]
 	div = [div(i, i) for i in range(11)]
-
+	All = [All(i) for i in range(11)]
+	
 	print(add)
 	print(sub)
 	print(mul)
 	print(div)
 	print("results")
+
+testLoop()
